2bff0efaf439063b31c86c222f836622af6de32e
Ba9A-tata <yogadzakkii@gmail.com>
2026-01-15 10:26:13 +0700

    buat kode untuk file auth.conroller.js

---
+++ backend/controllers/auth.controllers.js
@@ -0,0 +1,20 @@
+const db = require('../config/db')
+
+
+exports.login = (req, res) => {
+const { username, password } = req.body
+
+
+const sql = 'SELECT * FROM users WHERE username=? AND password=?'
+db.query(sql, [username, password], (err, result) => {
+if (err) return res.status(500).json(err)
+if (result.length === 0)
+return res.status(401).json({ message: 'Login gagal' })
+
+
+res.json({
+success: true,
+role: result[0].role
+})
+})
+}
